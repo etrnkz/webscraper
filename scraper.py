5fd4b94fcaf5e63a668b3ccdc83777366fc9cc12
etrnkz <elphador69@gmail.com>
2022-12-13 08:43:00 +0700

    fix: move credentials to env vars and fix code formatting issues 14:45

--- scraper.py
+++ scraper.py
@@ -1,36 +1,40 @@
 import requests 
 from bs4 import BeautifulSoup 
-from pyrogram import Client ,filters
+from pyrogram import Client, filters
 import os
 
-bot = Client(
-'my_bot',
-api_id =18802415,
-api_hash = 'a8993f96404fd9a67de867586b3ddc92',
-bot_token = '5772504388:AAEX0vjm-rx1sYdLU_qLpIS_RMqSvi5KANg')
+# Load credentials from environment variables
+API_ID = os.getenv('API_ID')
+API_HASH = os.getenv('API_HASH')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-@bot.on_message(filters.private & filters.command ('start'))
-def start (bot , msg):
-        msg.reply (  f'hello Sir{msg.from_user.first_name} i am webpage source code downloader bot just send me a link')
+bot = Client(
+    'my_bot',
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+@bot.on_message(filters.private & filters.command('start'))
+def start(bot, msg):
+    msg.reply(f'Hello {msg.from_user.first_name}! I am a webpage source code downloader bot. Just send me a link.')
 
 
 	
 @bot.on_message(filters.private & filters.regex("http"))
-def scrap (bot,msg):
+def scrap(bot, msg):
     url = msg.text 
-    request =requests.get(url)
-    soup = BeautifulSoup(request.content , 'html.parser')
-    parse = open( 'source-code.txt','w' ,encoding="utf-8")
+    request = requests.get(url)
+    soup = BeautifulSoup(request.content, 'html.parser')
+    parse = open('source-code.txt', 'w', encoding="utf-8")
     we = parse.write(soup.prettify())
-    parse.close
+    parse.close()
     msg.reply_document("source-code.txt")
 
        
 @bot.on_message(filters.private & filters.text)
-def show (bot , msg ):
-	msg.reply (text = " **your link must start from ◇◇♤◇◇http like as \n https://www.google.com \n \n \n for more feel free to contact the** : [Developer](https://t.me/e_phador)", disable_web_page_preview= True, quote = True)
-	
-	
+def show(bot, msg):
+    msg.reply(text="**Your link must start from http like:\nhttps://www.google.com\n\nFor more feel free to contact the** [Developer](https://t.me/e_phador)", disable_web_page_preview=True, quote=True)
 	    
     
-bot.run( )
+bot.run()
