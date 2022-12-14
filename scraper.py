d7e2edeb65caaf0e2f8f381564095e355c470ae8
etrnkz <elphador69@gmail.com>
2022-12-14 10:39:00 +0700

    feat: add comprehensive error handling and logging 11:20

--- scraper.py
+++ scraper.py
@@ -2,6 +2,14 @@ import requests
 from bs4 import BeautifulSoup 
 from pyrogram import Client, filters
 import os
+import logging
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
 
 # Load credentials from environment variables
 API_ID = os.getenv('API_ID')
@@ -23,13 +31,36 @@ def start(bot, msg):
 	
 @bot.on_message(filters.private & filters.regex("http"))
 def scrap(bot, msg):
-    url = msg.text 
-    request = requests.get(url)
-    soup = BeautifulSoup(request.content, 'html.parser')
-    parse = open('source-code.txt', 'w', encoding="utf-8")
-    we = parse.write(soup.prettify())
-    parse.close()
-    msg.reply_document("source-code.txt")
+    url = msg.text
+    logger.info(f"Processing URL request from user {msg.from_user.id}: {url}")
+    
+    try:
+        request = requests.get(url, timeout=30)
+        request.raise_for_status()
+        
+        soup = BeautifulSoup(request.content, 'html.parser')
+        
+        with open('source-code.txt', 'w', encoding="utf-8") as parse:
+            parse.write(soup.prettify())
+        
+        msg.reply_document("source-code.txt")
+        logger.info(f"Successfully sent source code for {url}")
+        
+        # Clean up temporary file
+        try:
+            os.remove('source-code.txt')
+        except Exception as e:
+            logger.warning(f"Failed to remove temp file: {e}")
+            
+    except requests.exceptions.Timeout:
+        msg.reply("⏱️ Request timed out. The website took too long to respond.")
+        logger.error(f"Timeout error for URL: {url}")
+    except requests.exceptions.RequestException as e:
+        msg.reply(f"❌ Failed to fetch the webpage. Error: {str(e)}")
+        logger.error(f"Request error for {url}: {e}")
+    except Exception as e:
+        msg.reply("❌ An unexpected error occurred while processing your request.")
+        logger.error(f"Unexpected error processing {url}: {e}")
 
        
 @bot.on_message(filters.private & filters.text)
